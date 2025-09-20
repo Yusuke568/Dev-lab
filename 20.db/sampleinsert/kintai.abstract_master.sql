867cfde61d37cb2d058dda68421c550497788626
n.yusuke <yusuke_030953334@yahoo.co.jp>
2025-09-21 00:04:30 +0900

    DB Createsql and SampleDataInsert

---
+++ 20.db/sampleinsert/kintai.abstract_master.sql
@@ -0,0 +1,10 @@
+insert into kintai.abstract_master(`ID`,`NAME`) values 
+    (1,'o‹Î')
+  , (2,'‹xo')
+  , (3,'—L‹‹')
+  , (4,'‘ã‹x')
+  , (5,'”¼‹x')
+  , (6,'“Á‹x')
+  , (7,'Uo')
+  , (8,'U‹x')
+  , (9,'‚»‚Ì‘¼');
---
+++ 20.db/sampleinsert/kintai.project_master.sql
@@ -0,0 +1,2 @@
+insert into kintai.project_master(`ID`,`NAME`) values 
+    (1,'Sample');

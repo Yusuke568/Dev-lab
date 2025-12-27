ee3cc8dc7261077d71400c9198148f1b2653d4a9
LifeOnFloor <nogawatete@gmail.com>
2025-12-27 17:15:16 +0900

    refactor: テーブル構造の変更 - abstract_masterテーブルの修正   - 区分カラムをDB上で意味が分かるように変更

--- 20.db/createtb/all_createtables.sql
+++ 20.db/createtb/all_createtables.sql
@@ -1,24 +1,36 @@
-CREATE TABLE `abstract_master` (
-  `ID` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '適用区分',
-  `NAME` varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '適用名称',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='適用マスタ'
-
+-- --------------------------------------------------------
+-- テーブル削除
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS abstract_master;
+DROP TABLE IF EXISTS login_table;
+DROP TABLE IF EXISTS work_month_table;
+DROP TABLE IF EXISTS staff_table;
+DROP TABLE IF EXISTS class_master;
+DROP TABLE IF EXISTS leave_grant_rules;
+-- --------------------------------------------------------
+-- 適用マスタ (abstract_master) 作成
+-- --------------------------------------------------------
+CREATE TABLE abstract_master (
+  ID varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '適用区分',
+  NAME varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '適用名称',
+  -- 集計用フラグカラム（新規追加）
+  IS_WORK int(1) DEFAULT 0 COMMENT '労働日フラグ(1：労働, 0：休み)',
+  IS_PAID tinyint(1) DEFAULT 0 COMMENT '有給対象フラグ(1：有給, 0：無給)',
+  IS_LEGAL_HOLIDAY tinyint(1) DEFAULT 0 COMMENT '法定休日フラグ(1：法定休日)',
+  IS_PRESCRIBED_HOLIDAY tinyint(1) DEFAULT 0 COMMENT '所定休日フラグ(1：所定休日)',
+  PRIMARY KEY (ID)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '適用マスタ';
+-- --------------------------------------------------------
+-- 役職マスタ (class_master) 作成
+-- --------------------------------------------------------
 CREATE TABLE `class_master` (
   `ID` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '役職区分',
   `NAME` varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '役職名称',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='役職マスタ'
-
-CREATE TABLE `login_table` (
-  `USERNAME` int NOT NULL COMMENT '社員ID',
-  `PASSWORD` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'パスワード',
-  `ENABLED` tinyint(1) DEFAULT NULL COMMENT '有効フラグ',
-  `EXPIRATION_DATE` date DEFAULT NULL COMMENT '有効期限',
-  PRIMARY KEY (`USERNAME`),
-  CONSTRAINT `login_table_ibfk_1` FOREIGN KEY (`USERNAME`) REFERENCES `staff_table` (`ID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='社員ログインテーブル'
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '役職マスタ';
+-- --------------------------------------------------------
+-- 社員テーブル (staff_table) 作成
+-- --------------------------------------------------------
 CREATE TABLE `staff_table` (
   `ID` int NOT NULL COMMENT '社員ID',
   `NAME` varchar(40) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '社員名',
@@ -30,8 +42,21 @@ CREATE TABLE `staff_table` (
   PRIMARY KEY (`ID`),
   KEY `CLASS_ID` (`CLASS_ID`),
   CONSTRAINT `staff_table_ibfk_1` FOREIGN KEY (`CLASS_ID`) REFERENCES `class_master` (`ID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='社員テーブル'
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '社員テーブル';
+-- --------------------------------------------------------
+-- 社員ログインテーブル (login_table) 作成
+-- --------------------------------------------------------
+CREATE TABLE `login_table` (
+  `USERNAME` int NOT NULL COMMENT '社員ID',
+  `PASSWORD` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'パスワード',
+  `ENABLED` tinyint(1) DEFAULT NULL COMMENT '有効フラグ',
+  `EXPIRATION_DATE` date DEFAULT NULL COMMENT '有効期限',
+  PRIMARY KEY (`USERNAME`),
+  CONSTRAINT `login_table_ibfk_1` FOREIGN KEY (`USERNAME`) REFERENCES `staff_table` (`ID`) ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '社員ログインテーブル';
+-- --------------------------------------------------------
+-- 勤怠月テーブル (work_month_table) 作成
+-- --------------------------------------------------------
 CREATE TABLE `work_month_table` (
   `STAFF_ID` int NOT NULL COMMENT '社員ID',
   `WORK_DATE` date NOT NULL COMMENT '日付',
@@ -47,14 +72,16 @@ CREATE TABLE `work_month_table` (
   `OVERTIME` int DEFAULT NULL COMMENT '残業時間',
   `ABSTRACT_ID` int DEFAULT NULL COMMENT '摘要区分',
   `REMARKS` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '備考',
-  PRIMARY KEY (`STAFF_ID`,`WORK_DATE`),
+  PRIMARY KEY (`STAFF_ID`, `WORK_DATE`),
   CONSTRAINT `work_month_table_ibfk_1` FOREIGN KEY (`STAFF_ID`) REFERENCES `staff_table` (`ID`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='勤怠月テーブル'
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '勤怠月テーブル';
+-- --------------------------------------------------------
+-- 有給付与ルールテーブル (leave_grant_rules) 作成
+-- --------------------------------------------------------
 CREATE TABLE `leave_grant_rules` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'ルールID',
   `years_of_service` int NOT NULL COMMENT '勤続年数',
   `grant_days` int NOT NULL COMMENT '付与日数',
   PRIMARY KEY (`id`),
   UNIQUE KEY `years_of_service` (`years_of_service`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='有給付与ルールテーブル';
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '有給付与ルールテーブル';
